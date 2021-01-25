b6850ff5d840c2bf83d4c4339a9488b3620bf45d
coco_momma <hwchae@mommaeat.com>
2021-01-25 15:48:03 +0900

    Medium Device - Header (버튼을 눌렀을때 메뉴가 나오게하는방법 javascript로 이벤트처리 해준다.)

---
+++ main.js
@@ -0,0 +1,16 @@
+(function (window, documnet) {
+    'use strict';
+
+    const $toggles = documnet.querySelectorAll('.toggle');
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function () {
+        toggleElements();
+    });
+
+    function toggleElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+})(window, document)
