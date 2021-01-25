c998afd93a6ca144cdd9ad123c646917109e419d
coco_momma <hwchae@mommaeat.com>
2021-01-25 16:08:38 +0900

    Medium Device - 리사이즈 이벤트 (화면 크기늘렸을때 toggle button텍스트 없애기, 사이즈 줄였을때 토글버튼 닫혀있는상태로 실행하는 방법)

--- main.js
+++ main.js
@@ -8,9 +8,22 @@
         toggleElements();
     });
 
+    window.addEventListener('resize', function(){
+        if(window.innerWidth>1024){
+            offElements();
+        }
+    })
+
     function toggleElements() {
         [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
         });
     }
+
+    function offElements(){
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });
+    }
+
 })(window, document)
